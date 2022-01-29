753f8592e389d03f869a1e007a998265881a8fa4
ClimberMel <mel.pryor@shaw.ca>
2022-01-29 12:56:55 -0800

    Add basic window DataEntry.py

---
+++ ClientEntry.py
@@ -0,0 +1,15 @@
+# ClientEntry.py
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+
+# root window
+root = tk.Tk()
+root.geometry('300x220')
+root.resizable(True, True)
+root.title('Client Entry')
+
+
+
+root.mainloop()
