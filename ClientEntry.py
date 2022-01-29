88e7d14e511eb5a5ab366aac5819bf7f63681451
ClimberMel <mel.pryor@shaw.ca>
2022-01-29 13:25:09 -0800

    Add comments to ClientyEntry.py

--- ClientEntry.py
+++ ClientEntry.py
@@ -1,15 +1,17 @@
 # ClientEntry.py
 
-import tkinter as tk
+# Import utilities
+#   
+import tkinter as tk            # you can access tkinter function using the abreviation tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
 
-# root window
-root = tk.Tk()
-root.geometry('300x220')
-root.resizable(True, True)
-root.title('Client Entry')
+# Creates the window called root
+root = tk.Tk()                  # creates a window instance called root.  you used the tk set above instead of tkinter.Tk()
+root.geometry('300x220')        # Initial size of window root
+root.resizable(True, True)      # Allows to resize horiz, vert
+root.title('Client Entry')      # Frame (window) title
 
 
-
+# This runs the main window
 root.mainloop()
