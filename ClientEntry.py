7b11bfa1d3792b927536cb7f630fc526468b1b46
ClimberMel <mel.pryor@shaw.ca>
2022-01-29 13:30:17 -0800

    More code documentation

--- ClientEntry.py
+++ ClientEntry.py
@@ -3,8 +3,8 @@
 # Import utilities
 #   
 import tkinter as tk            # you can access tkinter function using the abreviation tk
-from tkinter import ttk
-from tkinter.messagebox import showinfo
+from tkinter import ttk         # ttk is like tkinter, but has some additional commands and different graphics
+from tkinter.messagebox import showinfo     # this allows the showinfo function which displays a pop up
 
 # Creates the window called root
 root = tk.Tk()                  # creates a window instance called root.  you used the tk set above instead of tkinter.Tk()
